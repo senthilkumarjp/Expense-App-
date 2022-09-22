9543ce826dd38ddfa1141d45fdaaa06247436cb8
senthilkumarjp <96403664+senthilkumarjp@users.noreply.github.com>
2022-09-22 20:55:52 +0530

    Delete my-app/src/pages directory

--- my-app/src/pages/Products.js
+++
@@ -1,9 +0,0 @@
-function Products() {
-    return (
-        <div>
-            <h1>welcome in products</h1>
-        </div>
-    )
-}
-
-export default Products;
--- my-app/src/pages/Welcome.js
+++
@@ -1,9 +0,0 @@
-function Welcome() {
-    return (
-        <div>
-            <h1>welcome in component</h1>
-        </div>
-    )
-}
-
-export default Welcome;
